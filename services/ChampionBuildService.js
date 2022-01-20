aff5d0c6f41346f11464566669bab5a7431a5b5b
Naglis Bukauskas <naglisvbukauskas@gmail.com>
2022-01-20 00:24:49 -0500

    We're querying runes

--- services/ChampionBuildService.js
+++ services/ChampionBuildService.js
@@ -15,14 +15,50 @@ async function fetchAndProcessChampionData(message) {
 
 
     try {
+        console.log('URI: ' + uri);
         const browser = await puppeteer.launch();
         const [page] = await browser.pages();
 
         // await page.goto('https://u.gg/lol/champions/vayne/build',  { waitUntil: 'networkidle0' });
         //TODO: If there are any issues with reliability, it will most likely be here.
         await page.goto(uri);
-        const data = await page.evaluate(() => document.querySelector('*').outerHTML);
 
+        page.lo
+        // const data = await page.evaluate(() => document.querySelector('div.rune-tree_v2').outerHTML);
+        // const data = await page.evaluate((selector) => {
+        //     return {
+        //         body: document.querySelector(selector).outerHTML,
+        //     };
+        // }, 'div.rune-tree_v2');
+        const data = await page.evaluate((selector) => {
+            return {
+                // keystone: document.querySelector('div.perk.keystone.perk-active').innerHTML,
+                runes: Array.from(document.querySelectorAll('div.perk.perk-active')).map(x => x.innerHTML).splice(0, 6)
+
+            };
+        }, 'div.perk.keystone.perk-active');
+
+
+        // const data = await page.evaluateHandle(() => document.querySelector('div.rune-tree_v2'));
+        // console.log(await data.getProperties());
+
+
+        // const data = await page.$$(`${'div.rune-tree_v2'} > *`);
+
+        // const data = await page.evaluate(() => document.querySelector('div.rune-tree_v2').outerHTML).then((res) => {
+        //     console.log(r);
+        // })
+        // const data = await page.evaluate(() => document.querySelectorAll('div.perk'));
+
+        // let test = await page.evaluate(() => {
+        //     let elements = Array.from(document.querySelectorAll('div.rune-tree_v2'));
+        //     let links = elements.map(element => {
+        //         return element.outerHTML
+        //     })
+        //     return links;
+        // });
+        //
+        // console.log(test);
         console.log(data);
         await browser.close();
 
@@ -30,6 +66,41 @@ async function fetchAndProcessChampionData(message) {
         console.error(error);
     }
 }
+
+
+async function extractData(page) {
+    try {
+        const persons = await page.evaluate(() => {
+            let persons = [];
+            const personsElem = document.querySelectorAll("div.rune-tree_v2");
+            const personsElemLen = personsElem.length;
+
+            for (let i = 0; i < personsElemLen; i++) {
+                try {
+                    const personElem = personsElem[i];
+
+                    const photo = personElem.querySelector("img").src;
+
+                    const name = personElem.querySelector("h3").innerText;
+                    const email = personElem.querySelector(".email").innerText;
+                    const phone = personElem.querySelector(".phone").innerText;
+
+                    persons.push({ photo, name, services, price, ratings, reviews });
+                } catch (e) {}
+            }
+
+            return persons;
+        });
+
+
+
+        // do anything with persons
+
+        console.log(persons.length, "persons", persons);
+    } catch(e) {
+        console.error("Unable to extract persons data", e);
+    }
+};
 
 module.exports.championBuildService = {
     fetchAndProcessChampionData,
