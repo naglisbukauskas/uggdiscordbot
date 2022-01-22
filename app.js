600d5050119ae5f68997baacafef743975065d40
Naglis Bukauskas <naglisvbukauskas@gmail.com>
2022-01-22 14:05:10 -0500

    established mongodb connection, refactoring stuff now

--- app.js
+++ app.js
@@ -6,6 +6,25 @@ const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
 const {messageRouter} = require("./routers/MessageRouter");
 
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://discordbot:" + process.env.DATABASE_PASSWORD + "@botcluster.1j2yr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const databaseClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function run() {
+    try {
+        await databaseClient.connect();
+        const database = databaseClient.db('sample_mflix');
+        const movies = database.collection('movies');
+        // Query for a movie that has the title 'Back to the Future'
+        const query = { title: 'Back to the Future' };
+        const movie = await movies.findOne(query);
+        console.log(movie);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await databaseClient.close();
+    }
+}
+run().catch(console.dir);
+
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES]
