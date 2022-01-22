c66f7ce1926d1b07c8d4dad0057518123f4dc751
Naglis Bukauskas <naglisvbukauskas@gmail.com>
2022-01-21 20:22:27 -0500

    skill order

--- services/ChampionBuildService.js
+++ services/ChampionBuildService.js
@@ -39,8 +39,11 @@ async function fetchAndProcessChampionData(message) {
                 shards: (Array.from(document.querySelectorAll('div.shard.shard-active')).map(x => x.innerHTML).splice(0, 3)).map(x => x.slice(x.indexOf('alt=') + 5,x.indexOf(">") - 1)),
                 primaryTreeName: document.querySelector('div.rune-trees-container-2.media-query.media-query_MOBILE_LARGE__DESKTOP_LARGE > div > div.rune-tree_v2.primary-tree > div.rune-tree_header > div.perk-style-title').innerHTML,
                 secondaryTreeName: document.querySelector('div.rune-trees-container-2.media-query.media-query_MOBILE_LARGE__DESKTOP_LARGE > div.secondary-tree > div > div.rune-tree_v2 > div.rune-tree_header > div.perk-style-title').innerHTML,
+                skillOrder: Array.from(document.querySelectorAll('div.skill-path-container > div.skill-order-row > div.skill-order')).map(x => x.innerHTML),
             };
         });
+
+        console.log(data);
 
         let startingItemOptions = await page.$$('div[class="content-section content-section_no-padding recommended-build_items media-query media-query_MOBILE_SMALL__MOBILE_LARGE"] > div[class="content-section_content starting-items"] > div > div[class="item-row"] > div[class="item-dupe"] > div[class="item-img"]');
         let mythicCoreItemOptions = await page.$$('div[class="content-section content-section_no-padding recommended-build_items media-query media-query_MOBILE_SMALL__MOBILE_LARGE"] > div[class="content-section_content core-items mythic-border-container"] > div > div > div[class="item-row"] > div[class] > div[class="item-img"]');
@@ -52,6 +55,16 @@ async function fetchAndProcessChampionData(message) {
 
         // console.log('Primary Tree Name');
         // console.log(primaryTreeName);
+
+        let qLevelOrder = extractSkillOrder(data["skillOrder"][0]);
+        let wLevelOrder = extractSkillOrder(data["skillOrder"][1]);
+        let eLevelOrder = extractSkillOrder(data["skillOrder"][2]);
+        let rLevelOrder = extractSkillOrder(data["skillOrder"][3]);
+
+        data['qOrder'] = qLevelOrder;
+        data['wOrder'] = wLevelOrder;
+        data['eOrder'] = eLevelOrder;
+        data['rOrder'] = rLevelOrder;
 
         let startingItemsList = await evaluateImages(page, startingItemOptions);
         let mythicCoreItemsList = await evaluateImages(page, mythicCoreItemOptions);
@@ -152,6 +165,41 @@ function capitalizeChampionName(champion) {
     return champion;
 }
 
+function extractSkillOrder(skillList) {
+    let retList = [];
+    skillList = skillList.split("div");
+
+    for(let item of skillList) {
+        if(item.length > 6) {
+            if(item.includes("no")) {
+                retList.push("no-skill-up");
+            } else {
+                retList.push("skill-up");
+            }
+        }
+    }
+
+    return retList;
+
+
+    // let lvlArray = skillList.split('"');
+    // let i = 0;
+    //
+    // while (i < lvlArray.length) {
+    //     if (lvlArray[i].trim() !== "skill-up" && lvlArray[i] !== "no-skill-up") {
+    //         lvlArray.splice(i, 1);
+    //     } else if(lvlArray[i] === "skill-up ") {
+    //         lvlArray[i] = lvlArray[i].trim();
+    //         ++i;
+    //     } else {
+    //         ++i;
+    //     }
+    // }
+
+    // return lvlArray;
+
+}
+
 function extractRuneTreeColor(treeName) {
     treeName = treeName.toLowerCase();
     switch(treeName) {
@@ -187,9 +235,60 @@ function extractShardColor(shard) {
             return ':red_circle:';
         case 'magic resist':
             return ':purple_circle:';
-        case 'health':
+        case 'scaling bonus health':
             return ':green_circle:';
     }
+}
+
+function visualizeSkillOrder(skillArr) {
+    let naglis = skillArr;
+    for(let i = 0; i < naglis.length; i++) {
+        if(naglis[i] === 'no-skill-up') {
+            naglis[i] = ':white_large_square:';
+            // naglis[i] = ':white_small_square:';
+        } else {
+            switch(i + 1) {
+                case 1:
+                    naglis[i] = ':one:';
+                    break;
+                case 2:
+                    naglis[i] = ':two:';
+                    break;
+                case 3:
+                    naglis[i] = ':three:';
+                    break;
+                case 4:
+                    naglis[i] = ':four:';
+                    break;
+                case 5:
+                    naglis[i] = ':five:';
+                    break;
+                case 6:
+                    naglis[i] = ':six:';
+                    break;
+                case 7:
+                    naglis[i] = ':seven:';
+                    break;
+                case 8:
+                    naglis[i] = ':eight:';
+                    break;
+                case 9:
+                    naglis[i] = ':nine:';
+                    break;
+                case 10:
+                    naglis[i] = ':keycap_ten:';
+                    break;
+                default:
+                    naglis[i] = ':blue_square:'
+
+            }
+            // naglis[i] = ':large_blue_diamond:'
+            // naglis[i] = ':small_blue_diamond:';
+        }
+    }
+
+
+    return naglis;
 }
 
 function formatDataForResponse(data) {
@@ -205,6 +304,11 @@ function formatDataForResponse(data) {
     let secondShardColor = extractShardColor(shards[1]);
     let thirdShardColor = extractShardColor(shards[2]);
 
+    let QSO = visualizeSkillOrder(data['qOrder']);
+    let WSO = visualizeSkillOrder(data['wOrder']);
+    let ESO = visualizeSkillOrder(data['eOrder']);
+    let RSO = visualizeSkillOrder(data['rOrder']);
+
     const build = new MessageEmbed();
     build.setColor('#0099ff');
     build.setTitle(data['champion']);
@@ -215,7 +319,23 @@ function formatDataForResponse(data) {
         { name: "Keystone", value: primaryTreeColor + " " + runes[0]},
         { name: "Primary", value: primaryTreeColor + " " + runes[1] + '\n\n' + primaryTreeColor + " " + runes[2] + '\n\n' + primaryTreeColor + " " + runes[3], inline: true},
         { name: "Secondary", value: secondaryTreeColor + " " + runes[4] + '\n\n' + secondaryTreeColor + " " + runes[5], inline: true},
-        { name: "Shards", value: firstShardColor + " " + shards[0] + "\n\n" + secondShardColor + " " + shards[1] + "\n\n" + thirdShardColor + " "  + shards[2]}
+        { name: "Shards", value: firstShardColor + " " + shards[0] + "\n\n" + secondShardColor + " " + shards[1] + "\n\n" + thirdShardColor + " "  + shards[2]},
+
+        // {name: "Skill-Order", value: ":regional_indicator_q: " + QSO[0] + QSO[1] + QSO[2] + QSO[3] + QSO[4] + QSO[5] + QSO[6] + QSO[7] + QSO[8] + QSO[9] + QSO[10] + QSO[11] + QSO[12] + QSO[13] + QSO[14] + QSO[15] + "\n\n"
+        // + ":regional_indicator_w: " + WSO[0] + WSO[1] + WSO[2] + WSO[3] + WSO[4] + WSO[5] + WSO[6] + WSO[7] + WSO[8] + WSO[9] + WSO[10] + WSO[11] + WSO[12] + WSO[13] + WSO[14] + WSO[15]},
+        // {name: '\u200b', value: ":regional_indicator_e: " + ESO[0] + ESO[1] + ESO[2] + ESO[3] + ESO[4] + ESO[5] + ESO[6] + ESO[7] + ESO[8] + ESO[9] + ESO[10] + ESO[11] + ESO[12] + ESO[13] + ESO[14] + ESO[15] + "\n\n"
+        //         + ":regional_indicator_r: " + RSO[0] + RSO[1] + RSO[2] + RSO[3] + RSO[4] + RSO[5] + RSO[6] + RSO[7] + RSO[8] + RSO[9] + RSO[10] + RSO[11] + RSO[12] + RSO[13] + RSO[14] + RSO[15]},
+
+        {name: "Skill-Order", value: ":regional_indicator_q: " + QSO[0] + " " + QSO[1] + " " + QSO[2] + " " + QSO[3] + " " + QSO[4] + " " + QSO[5] + " " + QSO[6] + " " + QSO[7] + " " + QSO[8] + " " + QSO[9] + " " + QSO[10] + " " + QSO[11] + " " + QSO[12] + " " + QSO[13] + " " + QSO[14] + " " + QSO[15] + " " + QSO[16] + " " + QSO[17] + "\n\n"
+                + ":regional_indicator_w: " + WSO[0] + " " + WSO[1] + " " + WSO[2] + " " + WSO[3] + " " + WSO[4] + " " + WSO[5] + " " + WSO[6] + " " + WSO[7] + " " + WSO[8] + " " + WSO[9] + " " + WSO[10] + " " + WSO[11] + " " + WSO[12] + " " + WSO[13] + " " + WSO[14] + " " + WSO[15] + " " + WSO[16]+ " " + WSO[17]},
+        {name: '\u200b', value: ":regional_indicator_e: " + ESO[0] + " " + ESO[1] + " " + ESO[2] + " " + ESO[3] + " " + ESO[4] + " " + ESO[5] + " " + ESO[6] + " " + ESO[7] + " " + ESO[8] + " " + ESO[9] + " " + ESO[10] + " " + ESO[11] + " " + ESO[12] + " " + ESO[13] + " " + ESO[14] + " " + ESO[15] + " " + ESO[16] + " " + ESO[17] + "\n\n"
+                + ":regional_indicator_r: " + RSO[0] + " " + RSO[1] + " " + RSO[2] + " " + RSO[3] + " " + RSO[4] + " " + RSO[5] + " " + RSO[6] + " " + RSO[7] + " " + RSO[8] + " " + RSO[9] + " " + RSO[10] + " " + RSO[11] + " " + RSO[12] + " " + RSO[13] + " " + RSO[14] + " " + RSO[15] + " " + RSO[16] + " " + RSO[17]},
+
+
+        // + "E: " + ESO[0] + ESO[1] + ESO[2] + ESO[3] + ESO[4] + ESO[5] + ESO[6] + ESO[7] + ESO[8] + ESO[9] + ESO[10] + ESO[11] + ESO[12] + ESO[13] + ESO[14] + ESO[15] + "\n\n"
+        // + "R: " + RSO[0] + RSO[1] + RSO[2] + RSO[3] + RSO[4] + RSO[5] + RSO[6] + RSO[7] + RSO[8] + RSO[9] + RSO[10] + RSO[11] + RSO[12] + RSO[13] + RSO[14] + RSO[15]},
+
+
 
         // {name: '\u200b', value: '\u200b'},
         // { name: "\u200b", value: "<:yellow_square:933963482699825223>" + " " + runes[1], inline: true},
